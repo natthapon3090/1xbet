6726b7e47971be1d9130d3a35ef22e70253c64c3
natthapon3090 <garfield11kp@gmail.com>
2026-02-09 16:07:00 +0700

    Create login.py

---
+++ login.py
@@ -0,0 +1,12 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+
+class LoginScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        layout = BoxLayout(orientation="vertical", padding=30, spacing=15)
