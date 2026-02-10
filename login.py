1caafc18aa8d90e249f240ec408f7485c3ecdf33
natthapon3090 <garfield11kp@gmail.com>
2026-02-10 18:54:03 +0700

    Update login.py

--- login.py
+++ login.py
@@ -10,3 +10,19 @@ class LoginScreen(Screen):
         super().__init__(**kwargs)
 
         layout = BoxLayout(orientation="vertical", padding=30, spacing=15)
+        layout.add_widget(Label(text="⚽ Football Predictor", font_size=24))
+
+        self.username = TextInput(hint_text="Username", multiline=False)
+        self.password = TextInput(hint_text="Password", password=True, multiline=False)
+
+        login_btn = Button(text="Login")
+        login_btn.bind(on_press=self.login)
+
+        self.message = Label(text="")
+
+        layout.add_widget(self.username)
+        layout.add_widget(self.password)
+        layout.add_widget(login_btn)
+        layout.add_widget(self.message)
+
+        self.add_widget(layout)
