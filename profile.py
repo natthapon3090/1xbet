a3ec0a4d7c3fc9a797fb4e22ed4c759c2223dd03
natthapon3090 <garfield11kp@gmail.com>
2026-02-11 23:15:21 +0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -1,1 +1,9 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
 
+
+class ProfileScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
