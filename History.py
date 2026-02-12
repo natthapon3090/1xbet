0db3c22fc7632ebedfd6737b74b3d009e1239c52
wwwatchara2707-2549 <wwwatchara2707@gmail.com>
2026-02-12 22:57:54 +0700

    Update History.py

--- History.py
+++ History.py
@@ -8,4 +8,13 @@ class HistoryScreen(Screen):
     def on_pre_enter(self):
         self.clear_widgets()
         layout = BoxLayout(orientation="vertical", padding=30, spacing=10)
+        layout.add_widget(Label(text="📜 Betting History", font_size=24))
 
+        for item in self.manager.history:
+            layout.add_widget(Label(text=item))
+
+        back = Button(text="⬅ Back")
+        back.bind(on_press=lambda x: setattr(self.manager, "current", "dashboard"))
+
+        layout.add_widget(back)
+        self.add_widget(layout)
