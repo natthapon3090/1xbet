e42de544d7ed305625c3c895c3707a88cc86f792
wwwatchara2707-2549 <wwwatchara2707@gmail.com>
2026-02-12 22:53:07 +0700

    Update History.py

--- History.py
+++ History.py
@@ -1,1 +1,11 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class HistoryScreen(Screen):
+    def on_pre_enter(self):
+        self.clear_widgets()
+        layout = BoxLayout(orientation="vertical", padding=30, spacing=10)
 
