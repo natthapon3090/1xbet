fbc2acba0e1c35cce74f23ad940cb8fa8dca5b76
natthapon3090 <garfield11kp@gmail.com>
2026-02-12 22:42:27 +0700

    main.py

--- main.py
+++ main.py
@@ -2,20 +2,31 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager
 
 from login import LoginScreen
+from dashboard import DashboardScreen
 from league import LeagueScreen
 from match import MatchScreen
-from predict import PredictScreenw
+from predict import PredictScreen
 from profile import ProfileScreen
+from history import HistoryScreen
 
 
 class FootballApp(App):
     def build(self):
         sm = ScreenManager()
+
+        # Global Data
+        sm.balance = 1000
+        sm.history = []
+        sm.selected_match = ""
+
         sm.add_widget(LoginScreen(name="login"))
+        sm.add_widget(DashboardScreen(name="dashboard"))
         sm.add_widget(LeagueScreen(name="league"))
         sm.add_widget(MatchScreen(name="match"))
         sm.add_widget(PredictScreen(name="predict"))
         sm.add_widget(ProfileScreen(name="profile"))
+        sm.add_widget(HistoryScreen(name="history"))
+
         return sm
 
 
