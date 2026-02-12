2b5d55d78e942a8a2449c877eff8c2f52093f5e7
wwwatchara2707-2549 <wwwatchara2707@gmail.com>
2026-02-12 22:45:50 +0700

    Update wallet.py

--- wallet.py
+++ wallet.py
@@ -1,1 +1,12 @@
+class Wallet:
 
+    @staticmethod
+    def deposit(manager, amount):
+        manager.balance += amount
+
+    @staticmethod
+    def withdraw(manager, amount):
+        if amount <= manager.balance:
+            manager.balance -= amount
+            return True
+        return False
