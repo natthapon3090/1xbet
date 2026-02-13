23345cbdeb06ccfb17e770d2e5132b42903d7e65
natthapon3090 <garfield11kp@gmail.com>
2026-02-13 21:40:09 +0700

    Update bill.py

--- bill.py
+++ bill.py
@@ -1,1 +1,21 @@
+from kivy.uix.screenmanager import Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+
+
+class BillScreen(Screen):
+
+    def on_pre_enter(self):
+        self.clear_widgets()
+
+        layout = BoxLayout(
+            orientation="vertical",
+            padding=20,
+            spacing=10
+        )
+
+        layout.add_widget(Label(text="🧾 BETTING BILL", font_size=24))
+
+        bill = self.manager.current_bill
 
