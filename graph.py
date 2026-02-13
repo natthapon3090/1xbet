e33c976475b98299c1dd07517d5bd14f91da8899
natthapon3090 <garfield11kp@gmail.com>
2026-02-13 23:49:58 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1,1 +1,13 @@
+from kivy.uix.screenmanager import Screen
+import matplotlib.pyplot as plt
 
+
+class GraphScreen(Screen):
+
+    def on_pre_enter(self):
+        plt.plot(self.manager.profit_history)
+        plt.title("Balance History")
+        plt.xlabel("Rounds")
+        plt.ylabel("Balance")
+        plt.show()
+        self.manager.current = "dashboard"
