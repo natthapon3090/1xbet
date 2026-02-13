3554274885c2e5b9a912b22a51930d6cc690f2f3
natthapon3090 <garfield11kp@gmail.com>
2026-02-13 22:56:36 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,20 +1,32 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
-from style import GREEN
+from kivy.uix.label import Label
+from kivy.clock import Clock
+from style import NEON_GREEN, GOLD, BLUE
 
 
 class DashboardScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-
-        self.layout = BoxLayout(orientation="vertical", padding=30, spacing=20)
-
-        self.balance_label = Label(font_size=24)
-
-        
-        self.add_widget(self.layout)
 
     def on_pre_enter(self):
-        self.balance_label.text = f"💰 Balance: {self.manager.balance}"
+        self.clear_widgets()
+
+        layout = BoxLayout(orientation="vertical", padding=30, spacing=20)
+
+        self.balance_label = Label(font_size=24)
+        self.balance_label.text = f"💰 BALANCE: {self.manager.balance}"
+
+        self.live_label = Label(text="🔴 LIVE MATCH RUNNING", color=(1, 0, 0, 1))
+        Clock.schedule_interval(self.blink, 0.5)
+
+        btn1 = Button(text="⚽ BET NOW", background_color=NEON_GREEN)
+        btn1.bind(on_press=lambda x: setattr(self.manager, "current", "league"))
+
+        btn2 = Button(text="📜 BILLS")
+        btn2.bind(on_press=lambda x: setattr(self.manager, "current", "history"))
+
+        btn3 = Button(text="💳 DEPOSIT")
+        btn3.bind(on_press=lambda x: setattr(self.manager, "current", "deposit"))
+
+        btn4 = Button(text="📊 PROFIT GRAPH")
+        btn4.bind(on_press=lambda x: setattr(self.manager, "current", "graph"))
