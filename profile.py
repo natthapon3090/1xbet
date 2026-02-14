5187caab8aa5e95b0495095af6b8792881fd0996
reeweedometa-ux <reeweedometa@gmail.com>
2026-02-14 23:23:42 +0700

    Update profile.py

--- profile.py
+++ profile.py
@@ -8,17 +8,17 @@ class ProfileScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        self.layout = BoxLayout(orientation="vertical", padding=30, spacing=20)
+        layout = BoxLayout(orientation="vertical", padding=30, spacing=20)
 
         self.balance_label = Label(font_size=24)
 
-        btn = Button(text="⬅ Back to Dashboard")
-        btn.bind(on_press=lambda x: setattr(self.manager, "current", "dashboard"))
+        back = Button(text="⬅ Back")
+        back.bind(on_press=lambda x: setattr(self.manager, "current", "dashboard"))
 
-        self.layout.add_widget(self.balance_label)
-        self.layout.add_widget(btn)
+        layout.add_widget(self.balance_label)
+        layout.add_widget(back)
 
-        self.add_widget(self.layout)
+        self.add_widget(layout)
 
     def on_pre_enter(self):
         self.balance_label.text = f"💰 Current Balance: {self.manager.balance}"
