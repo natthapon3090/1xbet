90b8749cf700524382c2e0ec6faed0959ed87b63
wwwatchara2707-2549 <wwwatchara2707@gmail.com>
2026-02-14 22:11:11 +0700

    Update match.py

--- match.py
+++ match.py
@@ -1,20 +1,15 @@
 from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
+from kivy.uix.togglebutton import ToggleButton
 from kivy.uix.button import Button
+from kivy.uix.label import Label
+import random
 
 
 class MatchScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
 
-        self.matches = [
-            "Manchester City vs Arsenal",
-            "Liverpool vs Chelsea",
-            "Manchester United vs Tottenham"
-        ]
-
-        layout = BoxLayout(orientation="vertical", padding=30, spacing=20)
-        layout.add_widget(Label(text="🔥 Choose Match", font_size=24))
+    def on_pre_enter(self):
+        self.clear_widgets()
+        self.manager.selected_matches = []
 
        
